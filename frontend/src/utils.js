e2e220318ed7c246e663cd31fc75d9b9c895304c
Manoharan Thushantha Sanju <thushanthasanju8@gmail.com>
2021-10-10 10:02:18 +0530

    Initialize Rate-and-Review-products

---
+++ frontend/src/utils.js
@@ -0,0 +1,43 @@
+export const prices = [
+    {
+      name: 'Any',
+      min: 0,
+      max: 500000,
+    },
+    {
+      name: `Rs.100.00 to Rs.1000.00`,
+      min: 100,
+      max: 1000,
+    },
+    {
+      name: `Rs.1000.00 to Rs.10000.00`,
+      min: 1000,
+      max: 10000,
+    },
+    {
+      name: `Rs.10000.00 to Rs.500000.00`,
+      min: 10000,
+      max: 500000,
+    },
+  ];
+  export const ratings = [
+    {
+      name: '4stars & up',
+      rating: 4,
+    },
+  
+    {
+      name: '3stars & up',
+      rating: 3,
+    },
+  
+    {
+      name: '2stars & up',
+      rating: 2,
+    },
+  
+    {
+      name: '1stars & up',
+      rating: 1,
+    },
+  ];
