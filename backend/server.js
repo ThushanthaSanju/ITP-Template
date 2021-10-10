ecd6a9dfbd005f520272a982b092d2663109621c
Manoharan Thushantha Sanju <thushanthasanju8@gmail.com>
2021-10-10 10:02:04 +0530

    Initialize Create-mongo-db-setup-and-implement

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+
+import mongoose from "mongoose";
+import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
+
+const app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URL ||
+    "mongodb+srv://it20281564:it20281564@itp.hii6k.mongodb.net/KINGSHIP?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.use("/api/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is Ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
+});
