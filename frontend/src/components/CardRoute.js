920e2574385b0db5852f404de8d380d310720dc9
Manoharan Thushantha Sanju <thushanthasanju8@gmail.com>
2021-10-10 10:02:27 +0530

    Initialize edit-frontend

---
+++ frontend/src/components/CardRoute.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import Rating from "./Rating";
+
+// export default function Card(props) {
+//   const { card } = props;
+//   return (
+//     <div key={card._id} className="card">
+//       <div className="card-body">
+//         <Link to={`/card/${card._id}`}>
+//           <h2>{card.cardNumber}</h2>
+//         </Link>
+//         <div className="row">
+//           <div className="price">{card.price}.00</div>
+//           <div>
+//             <Link to={`/seller/${card.seller._id}`}>
+//               {card.seller.seller.name}
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
